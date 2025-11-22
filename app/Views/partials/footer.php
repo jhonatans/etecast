734c1c868a5b135345d5a2edfacf94c96218e7e6
jhonatans <j.rennan@outlook.com>
2025-11-22 15:45:26 -0300

    ajuste css e logos footer

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -2,7 +2,19 @@
 
 <footer class="footer mt-auto py-3 bg-light">
     <div class="container text-center">
-        <span class="text-muted">ETECast &copy; 2025 - Plataforma Central de Mídia e Streaming Educacional</span>
+
+        <span class="text-muted d-block mb-2">
+            ETECast © 2025 - Plataforma Central de Mídia e Streaming Educacional
+        </span>
+
+        <div class="footer-logos">
+            <img src="/img/etecast_logo.png" alt="ETECast Logo">
+            <img src="/img/etemac.png" alt="Etemac Logo">
+            <img src="/img/gre.png" alt="GRE Metro Sul Logo">
+            <img src="/img/estado.png" alt="Estado de Pernambuco Logo">
+            <img src="/img/facepe.png" alt="Facepe Logo">
+        </div>
+
     </div>
 </footer>
 
